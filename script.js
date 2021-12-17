40e40e8f6d3715576fbd07182b419307929b1a3a
dinahbrito <dinahbrito@gmail.com>
2021-12-16 19:55:53 -0800

    JS shows error message when input empty

--- script.js
+++ script.js
@@ -1,19 +1,61 @@
-const submit = document.getElementById('submit');
+const form = document.getElementById('form');
+const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
-const validate = (e) =>
+
+form.addEventListener('submit', e =>
 {
     e.preventDefault();
-    const firstname = document.getElementById('firstname');
-    const lastname = document.getElementById('lastname');
 
-    if (firstname.value === "") {
-        document.getElementById('error').style.display = 'grid';
-        document.getElementById('error-icon').style.display = 'block';
+    checkValue();
+});
+
+function checkValue()
+{
+    const firstNameValue = firstName.value;
+    const lastNameValue = lastName.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+
+
+    if (firstNameValue === "") {
+        addError(firstName, 'First name cannot be blank');
     }
 
+    if (lastNameValue === "") {
+        addError(lastName, 'Last name cannot be blank');
+    }
+
+    if (emailValue === "") {
+        //add error class
+        addError(email, 'Email cannot be blank');
+
+    } else if (!validEmail(emailValue)) {
+        //add different error class
+        addError(email, 'Please enter a valid email');
+    }
+
+    if (passwordValue === "") {
+        //add error class
+        addError(password, 'Password cannot be blank');
+    }
 
 }
 
+function addError(input, message)
+{
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+    formControl.className = 'form-control error';
+}
 
 
-submit.addEventListener('click', validate);
+function validEmail(email)
+{
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
